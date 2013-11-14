97a6139656df4aa7924ccf699c9c02a3cc32280b
vkinoshita <nct.shin@gmail.com>
2013-11-14 19:12:24 -0200

    traducao para portugues

---
+++ funcao_de_destino.m
@@ -0,0 +1,5 @@
+%Definido os vjs
+
+vj = @(j, x) sin((j * pi * x) / l)
+
+dvjdx = @(j, x) cos((j * pi * x) / l)
--- main.m
+++ main.m
@@ -1,2 +1,26 @@
-input1
-garlekin
+%EXERCICIO PROGRAMA
+%MAP2220
+%PROBLEMA DE STURM-LIOUVILLE - METODO DE GALERKIN
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+n = 20
+
+% intervalo
+a = 0
+b = pi
+l = b - a
+
+y = inline('(exp(x))*(sin(x))')
+
+
+% implementacao do primeiro item
+
+target_functions
+input_functions_1
+simpson(a, b, n, vj)
+
+% implementacao do segundo item
+
+target_functions
+input_functions_1
+simpson(a, b, n, vj)
