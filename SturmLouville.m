9712c75cbe40c47d3948c842b0c9c4cca6034c6a
vkinoshita <nct.shin@gmail.com>
2013-11-19 19:36:01 -0200

    pequenas mudancas

--- SturmLouville.m
+++ SturmLouville.m
@@ -3,12 +3,16 @@
 %PROBLEMA DE STURM-LIOUVILLE - METODO DE GALERKIN
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-entrada = entrada_principal();
+1;
 
-tamanho_intervalo = entrada.intervalo_fim - entrada.intervalo_inicio;
-vj = vj_aproximador(tamanho_intervalo);
+function SturmLiouville(_modelo_informacoes_sturm_liouville)
+	_modelo_entrada = _modelo_informacoes_sturm_liouville.entrada;
+	_modelo_funcao_aproximadora = _modelo_informacoes_sturm_liouville.funcao_aproximadora;
+	_modelo_funcoes_coeficientes = _modelo_informacoes_sturm_liouville.funcoes_coeficientes;
+	p = _modelo_funcoes_coeficientes.p;
+	q = _modelo_funcoes_coeficientes.q;
+	f = _modelo_funcoes_coeficientes.f;
 
-function principal(_modelo_entrada,_modelo_funcao_aproximadora,vj,dvjdx,p,q,f)
 	A = func_montar_matriz_interpoladora(_modelo_entrada, _modelo_funcao_aproximadora,p,q);
 	B = func_montar_resposta_interpoladora(_modelo_entrada,_modelo_funcao_aproximadora,f);
 	resultado = B / A;
@@ -20,6 +24,7 @@ function principal(_modelo_entrada,_modelo_funcao_aproximadora,vj,dvjdx,p,q,f)
 	end
 	
 	hold on;
+	vj = _modelo_funcao_aproximadora.funcao;
 	plot(dominio,func_aproximada(resultado,vj,dominio));
 endfunction
 
@@ -30,21 +35,33 @@ function resultado = func_aproximada(coef,vj,x)
 	end
 endfunction
 
-x = entrada.intervalo_inicio:0.01:pi;
-plot(x,entrada.funcao_exata(x));
+
+entrada = entrada_principal();
+tamanho_intervalo = entrada.intervalo_fim - entrada.intervalo_inicio;
+vj = vj_aproximador(tamanho_intervalo);
+
+
+infos = modelo_informacoes_sturm_liouville();
+infos.entrada = entrada;
+infos.funcao_aproximadora = vj;
 
 printf("\n*******************************************\n");
 printf("************* ENTRADA 1 *******************\n");
 printf("*******************************************\n");
 
-funcoes = funcoes_coeficientes_1();
+infos.funcoes_coeficientes = funcoes_coeficientes_1();
 
-principal(entrada,vj,vj.funcao,vj.derivada_da_funcao_em_x,funcoes.p,funcoes.q,funcoes.f);
+SturmLiouville(infos);
 
 printf("\n*******************************************\n");
 printf("************* ENTRADA 2 *******************\n");
 printf("*******************************************\n");
 
-funcoes = funcoes_coeficientes_2();
+infos.funcoes_coeficientes = funcoes_coeficientes_2();
 
-principal(entrada,vj,vj.funcao,vj.derivada_da_funcao_em_x,funcoes.p,funcoes.q,funcoes.f);
+SturmLiouville(infos);
+
+
+
+x = entrada.intervalo_inicio:0.01:pi;
+plot(x,entrada.funcao_exata(x));
