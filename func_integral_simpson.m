bbd26075e4f6e64fe6080759fffe8a486d105df4
vkinoshita <nct.shin@gmail.com>
2013-11-28 16:47:04 -0200

    usado o linspace e um bkp removido

--- func_integral_simpson.m
+++ func_integral_simpson.m
@@ -1,17 +1,17 @@
 function integral = func_integral_simpson(_intervalo, funcao_a_ser_integrada)
-	h = (_intervalo.fim - _intervalo.inicio)/_intervalo.particoes_numericas;
-	x = _intervalo.inicio:((_intervalo.fim - _intervalo.inicio) / _intervalo.particoes_numericas):_intervalo.fim;
-	
-	for i = 2:length(x) - 1
+	dominio_de_integracao = linspace(_intervalo.inicio, _intervalo.fim, _intervalo.particoes_numericas + 1);
+
+	for i = 2:length(dominio_de_integracao) - 1
 		if mod(i, 2) == 0
-			fx(i) = 4 * funcao_a_ser_integrada(x(i)); 
+			fx(i) = 4 * funcao_a_ser_integrada(dominio_de_integracao(i)); 
 		else
-			fx(i) = 2 * funcao_a_ser_integrada(x(i)); 
+			fx(i) = 2 * funcao_a_ser_integrada(dominio_de_integracao(i)); 
 		end 
 	end
 
-	integral = funcao_a_ser_integrada(x(1)) + funcao_a_ser_integrada(x(length(x))) + sum(fx);
+	integral = funcao_a_ser_integrada(dominio_de_integracao(1)) + funcao_a_ser_integrada(dominio_de_integracao(length(dominio_de_integracao))) + sum(fx);
 
+	h = _intervalo.tamanho/_intervalo.particoes_numericas;
 	integral = h/3 * integral; 
 
 endfunction
